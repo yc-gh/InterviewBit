e1df3a9333f510452bf6c5e9247d884069568daf
yc-gh <ycpcmr@gmail.com>
2020-04-21 11:44:51 +0530

    Added files

---
+++ Interview Questions/Trees/Lowest common ancestor.java
@@ -0,0 +1,44 @@
+// https://www.interviewbit.com/problems/least-common-ancestor/
+
+/**
+ * Definition for binary tree
+ * class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) {
+ *      val = x;
+ *      left=null;
+ *      right=null;
+ *     }
+ * }
+ */
+public class Solution {
+    public int lca(TreeNode A, int B, int C) {
+        int[] found = new int[1];
+        int res = lcaUtil(A, B, C, found);
+        return (found[0]==2) ? res : -1;
+    }
+    
+    private static int lcaUtil(TreeNode A, int B, int C, int[] found)
+    {
+        if(A==null) return -1;
+        int foundhere = -1;
+        if(A.val==B) 
+        {
+            found[0]++;
+            foundhere = A.val;
+        }
+        if(A.val==C)
+        {
+            found[0]++;
+            foundhere = A.val;
+        }
+        
+        int left = lcaUtil(A.left, B, C, found);
+        int right = lcaUtil(A.right, B, C, found);
+        if(foundhere != -1) return foundhere;
+        if(left != -1 && right!= -1) return A.val;
+        return left!=-1 ? left : right;
+    }
+}
