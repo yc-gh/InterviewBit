fbb9f1a079059530c2d1d7a2ab4c344ba471d663
yc-gh <ycpcmr@gmail.com>
2020-04-27 22:44:27 +0530

    Added files

---
+++ Interview Questions/String/AtoI.java
@@ -0,0 +1,22 @@
+// https://www.interviewbit.com/problems/atoi/
+
+public class Solution {
+    // DO NOT MODIFY THE LIST. IT IS READ ONLY
+    public int atoi(final String str) {
+        int n = str.length();
+        if(n==0) return 0;
+        int sign=1, num=0, i=0, IntMax=Integer.MAX_VALUE, IntMin=Integer.MIN_VALUE;
+        while(i<n && str.charAt(i)==' ') i++;
+        if(i<n && (str.charAt(i)=='+' || str.charAt(i)=='-'))
+            sign = str.charAt(i++)=='+' ? 1 : -1;
+        while(i<n && str.charAt(i)>='0' && str.charAt(i)<='9')
+        {
+            if(num>IntMax/10 || (num==IntMax/10 && (str.charAt(i)-'0'>IntMax%10)))
+            {
+                return sign==1 ? IntMax : IntMin;
+            }
+            num = num * 10 + (str.charAt(i++) -'0');
+        }
+        return num * sign;
+    }
+}
