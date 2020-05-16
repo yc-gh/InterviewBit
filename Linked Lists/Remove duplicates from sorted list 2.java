84ea73b8fa134bb755fd8fb1500b559810b0cab9
yc-gh <ycpcmr@gmail.com>
2020-05-16 09:30:21 +0530

    Added files

---
+++ Linked Lists/Remove duplicates from sorted list 2.java
@@ -0,0 +1,23 @@
+// https://www.interviewbit.com/problems/remove-duplicates-from-sorted-list-ii/
+
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null)
+            return null;
+        ListNode start = new ListNode(0);
+        ListNode prev = start;
+        ListNode curr = head, next = head.next;
+        while (curr != null) {
+            while (next != null && next.val == curr.val) {
+                next = next.next;
+            }
+            if (next == curr.next) {
+                prev.next = curr;
+                prev = prev.next;
+            }
+            curr = next;
+        }
+        prev.next = null;
+        return start.next;
+    }
+}
