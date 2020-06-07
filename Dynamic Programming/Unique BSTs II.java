adbbb2561a9790d17101fa00d86eded1d4c0bf7e
yc-gh <36479105+yc-gh@users.noreply.github.com>
2020-06-07 12:16:46 +0530

    Create Unique BSTs II.java

---
+++ Dynamic Programming/Unique BSTs II.java
@@ -0,0 +1,12 @@
+//https://www.interviewbit.com/problems/unique-binary-search-trees-ii/
+
+public class Solution {
+    public int numTrees(int n) {
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        for(int level=1; level<=n; level++)
+            for(int root=1; root<=level; root++)
+                dp[level] += dp[root-1]*dp[level-root];
+        return dp[n];
+    }
+}
