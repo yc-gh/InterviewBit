0ca4f0bfbeb742748e451aa7033e111ed00dbdaf
yc-gh <36479105+yc-gh@users.noreply.github.com>
2020-06-13 19:12:11 +0530

    Create Sum of pairwise hamming distance.java

---
+++ Math/Sum of pairwise hamming distance.java
@@ -0,0 +1,21 @@
+//
+
+public class Solution {
+    // DO NOT MODIFY THE ARGUMENTS WITH "final" PREFIX. IT IS READ ONLY
+    public int hammingDistance(final int[] arr) {
+        int n = arr.length;
+        int ans = 0;
+  
+        for (int i = 0; i < 32; i++) { 
+            int count = 0;
+            
+            for (int j = 0; j < n; j++) 
+                if ((arr[j] & (1 << i)) == 0) count++; 
+                
+            ans += (2L * count * (n - count))%1000000007; 
+            ans %= 1000000007;
+        } 
+          
+        return ans;
+    }
+}
