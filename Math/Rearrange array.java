0f663e8a9963f85436a94b1078f3200dcc038004
yc-gh <36479105+yc-gh@users.noreply.github.com>
2020-06-13 10:17:08 +0530

    Create Rearrange array.java

---
+++ Math/Rearrange array.java
@@ -0,0 +1,16 @@
+//https://www.interviewbit.com/problems/rearrange-array/
+
+public class Solution {
+    public void arrange(ArrayList<Integer> a) {
+        int n = a.size();
+        for(int i=0; i<n; i++)
+        {
+            a.set(i, a.get(i)+ (a.get(a.get(i))%n)*n);
+        }
+        
+        for(int i=0; i<n; i++)
+        {
+            a.set(i, a.get(i)/n);
+        }
+    }
+}
