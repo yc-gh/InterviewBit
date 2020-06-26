9b79d5dee85689ccd6a9e6475aefbfbfee0ac8eb
yc-gh <36479105+yc-gh@users.noreply.github.com>
2020-06-26 13:18:25 +0530

    Create Word Ladder I.java

---
+++ Graphs/Word Ladder I.java
@@ -0,0 +1,58 @@
+//https://www.interviewbit.com/problems/word-ladder-i/
+
+public class Solution {
+    public int solve(String beginWord, String endWord, ArrayList<String> wordList) {
+        Set<String> wordSet = new HashSet<>(wordList);
+        if(!wordSet.contains(endWord)) return 0;
+        
+        Set<String> startSet = new HashSet<>(), endSet = new HashSet<>();
+        Set<String> visited = new HashSet<>();
+        
+        int len = 1;
+        
+        startSet.add(beginWord);
+        endSet.add(endWord);
+        
+        while(!startSet.isEmpty())
+        {
+            Set<String> tempSet = new HashSet<>(); 
+            
+            for(String word : startSet)
+            {
+                char[] chars = word.toCharArray();
+                for(int i=0; i<chars.length; i++)
+                {
+                    for(char c='a'; c<='z'; c++)
+                    {
+                        char old = chars[i];
+                        chars[i] = c;
+                        
+                        String newWord = String.valueOf(chars);
+                        
+                        if(endSet.contains(newWord)) return len+1;
+                        
+                        if(!visited.contains(newWord) && wordSet.contains(newWord))
+                        {
+                            tempSet.add(newWord);
+                            visited.add(newWord);
+                        }
+                        
+                        chars[i] = old;
+                    }
+                }
+            }
+            
+            //Choose smaller of temp or end as next start 
+            if(tempSet.size() > endSet.size())
+            {
+                startSet = endSet;
+                endSet = tempSet;
+            }
+            else startSet = tempSet;
+            
+            len++;
+        }
+        
+        return 0;
+    }
+}
