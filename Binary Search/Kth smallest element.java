bc16fb04599ca9f7fa8c60950adc2a9cd72da857
yc-gh <36479105+yc-gh@users.noreply.github.com>
2020-07-01 11:46:27 +0530

    Update Kth smallest element.java

--- Binary Search/Kth smallest element.java
+++ Binary Search/Kth smallest element.java
@@ -30,3 +30,36 @@ public class Solution {
         return count;
     }
 }
+
+
+//Alternative approach
+public class Solution {
+    // DO NOT MODIFY THE ARGUMENTS WITH "final" PREFIX. IT IS READ ONLY
+    public int kthsmallest(final int[] A, int B) {
+        int l=A[0], r=A[0];
+        
+        for(int i : A) 
+        {
+            if(i < l) l = i;
+            if(i > r) r = i;
+        }
+        
+        B--;
+        while(l < r)
+        {
+            int mid = (l+r>>>1)+1;
+            int count = getCount(A,mid);
+            if(count > B) r=mid-1;
+            else l=mid;
+        }
+        
+        return l;
+    }
+    
+    private static int getCount(final int[] A, int ele)
+    {
+        int count = 0;
+        for(int i : A) if(i<ele) count++;
+        return count;
+    }
+}
